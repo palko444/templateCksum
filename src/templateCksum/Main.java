5004d4fad056ccd099b9a6783f989e3b9563e902
palko444 <stanka@ehs.sk>
2017-03-14 22:26:04 +0100

    wip

--- src/templateCksum/Main.java
+++ src/templateCksum/Main.java
@@ -3,20 +3,20 @@ package templateCksum;
 import java.io.File;
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+	// TODO Auto-generated method stub
 
-		
-		Template t = new Template();
-		t.createTempFolder();
-		System.out.println(t.tempFolder);
-		t.downloadPolicies(args[0]);
-		File[] dataFiles = t.getDataFiles(t.tempFolder);
-		t.calculateMd5(dataFiles);
-		
-	}
+	Template t = new Template();
+	t.createTempFolder();
+	System.out.println(t.tempFolder);
+	t.downloadPolicies(args[0]);
+	File[] dataFiles = Template.getDataFiles(t.tempFolder);
+	HashMap<String, String> map = Template.calculateSha256(dataFiles);
+
+    }
 
 }
--- src/templateCksum/Template.java
+++ src/templateCksum/Template.java
@@ -9,7 +9,7 @@ import java.nio.file.Paths;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.HashMap;
 
 import org.joda.time.Duration;
 
@@ -18,69 +18,71 @@ import com.dxc.command_executor.StreamGobbler.QueueFullException;
 
 public class Template {
 
-	String tempFolder;
+    String tempFolder;
 
-	public void createTempFolder() throws IOException {
-		tempFolder = Files.createTempDirectory(Paths.get("/tmp"), "polDown").toString();
+    public void createTempFolder() throws IOException {
+	this.tempFolder = Files.createTempDirectory(Paths.get("/tmp"), "polDown").toString();
 
-	}
+    }
 
-	public void downloadPolicies(String pg) throws QueueFullException, IOException {
-		CommandExecutor.create("/opt/OV/bin/OpC/utils/opctempl", "-download", "pol_group=" + pg, "dir=" + tempFolder)
-				.exec(Duration.standardSeconds(30));
-	}
+    public void downloadPolicies(String pg) throws QueueFullException, IOException {
+	CommandExecutor.create("/opt/OV/bin/OpC/utils/opctempl", "-download", "pol_group=" + pg, "dir=" + this.tempFolder).exec(Duration.standardSeconds(30));
+    }
 
-	public void deleteTempFolder(String tmpFolder) {
+    public static void deleteTempFolder(String tmpFolder) {
 
-		File tmp = new File(tmpFolder);
-		if (tmp.exists()) {
-			File[] files = tmp.listFiles();
-			if (null != files) {
-				for (File file : files) {
-					if (file.isDirectory()) {
-						deleteTempFolder(file.toString());
-					} else {
-						file.delete();
-					}
-				}
-			}
-			tmp.delete();
+	File tmp = new File(tmpFolder);
+	if (tmp.exists()) {
+	    File[] files = tmp.listFiles();
+	    if (null != files) {
+		for (File file : files) {
+		    if (file.isDirectory()) {
+			deleteTempFolder(file.toString());
+		    } else {
+			file.delete();
+		    }
 		}
-	}
-
-	public File[] getDataFiles(String folder) {
-		File f = new File(folder);
-		return f.listFiles(new FilenameFilter() {
-
-			@Override
-			public boolean accept(File dir, String name) {
-				// TODO Auto-generated method stub
-				return name.endsWith("_data");
-			}
-		});
-	}
-
-	public void calculateMd5(File[] dataFiles) throws NoSuchAlgorithmException, IOException {
-		MessageDigest md = MessageDigest.getInstance("SHA-256");
-		for (File file : dataFiles) {
-			try (InputStream is = Files.newInputStream(file.toPath());
-					DigestInputStream dis = new DigestInputStream(is, md)) {
-				/* Read decorated stream (dis) to EOF as normal... */
-			}
-			byte[] digest = md.digest();
-			System.out.println("file: " + file + " cksum: " + bytesToHex(digest));
-		}
-	}
-	
-	final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
-	public static String bytesToHex(byte[] bytes) {
-	    char[] hexChars = new char[bytes.length * 2];
-	    for ( int j = 0; j < bytes.length; j++ ) {
-	        int v = bytes[j] & 0xFF;
-	        hexChars[j * 2] = hexArray[v >>> 4];
-	        hexChars[j * 2 + 1] = hexArray[v & 0x0F];
 	    }
-	    return new String(hexChars);
+	    tmp.delete();
 	}
-	
+    }
+
+    public static File[] getDataFiles(String folder) {
+	File f = new File(folder);
+	return f.listFiles(new FilenameFilter() {
+
+	    @Override
+	    public boolean accept(File dir, String name) {
+		// TODO Auto-generated method stub
+		return name.endsWith("_data");
+	    }
+	});
+    }
+
+    public static HashMap<String, String> calculateSha256(File[] dataFiles) throws NoSuchAlgorithmException, IOException {
+	HashMap<String, String> cksumFile = new HashMap<String, String>();
+	MessageDigest md = MessageDigest.getInstance("SHA-256");
+	for (File file : dataFiles) {
+	    try (InputStream is = Files.newInputStream(file.toPath()); DigestInputStream dis = new DigestInputStream(is, md)) {
+		/* Read decorated stream (dis) to EOF as normal... */
+	    }
+	    byte[] digest = md.digest();
+	    cksumFile.put(file.toString(), bytesToHex(digest));
+	    // System.out.println("file: " + file + " cksum: " + bytesToHex(digest));
+	}
+	return cksumFile;
+    }
+
+    final protected static char[] hexArray = "0123456789ABCDEF".toCharArray();
+
+    public static String bytesToHex(byte[] bytes) {
+	char[] hexChars = new char[bytes.length * 2];
+	for (int j = 0; j < bytes.length; j++) {
+	    int v = bytes[j] & 0xFF;
+	    hexChars[j * 2] = hexArray[v >>> 4];
+	    hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+	}
+	return new String(hexChars);
+    }
+
 }
